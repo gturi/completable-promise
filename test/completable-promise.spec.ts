321c667b1bc9532ad84840ef11e0cdfeaa6b4629
FlamingTuri <giacomo.venturini3@gmail.com>
2021-01-09 18:15:01 +0100

    add asynchronous behaviour test

--- test/completable-promise.spec.ts
+++ test/completable-promise.spec.ts
@@ -16,5 +16,38 @@ describe('CompletablePromise', () => {
 
       promise.resolve(number);
     });
+
+    it('should respect asynchronous behaviour', (done) => {
+      const array = ['foo'];
+      const promise = new CompletablePromise<string[]>();
+
+      promise.then((value: string[]) => {
+        expect(value).to.eql(['foo', 'bar', 'baz']);
+        done();
+      });
+
+      promise.resolve(array);
+
+      array.push('bar');
+      expect(array).to.eql(['foo', 'bar']);
+      array.push('baz');
+      expect(array).to.eql(['foo', 'bar', 'baz']);
+    });
+  });
+
+  describe('#reject', () => {
+    it('should return the value to the chained promise', (done) => {
+      const errorMessage = 'something went wrong';
+      const promise = new CompletablePromise();
+
+      promise.then(() => {
+        throw new Error('code should not be reached');
+      }).catch(reason => {
+        expect(reason).to.equal(errorMessage);
+        done();
+      });
+
+      promise.reject(errorMessage);
+    });
   });
 });
