7bb399e7f964275e0ce1b5988e79e3c641d56d3d
FlamingTuri <giacomo.venturini3@gmail.com>
2021-01-09 15:36:36 +0100

    add first implementation of completable promise

--- src/completable-promise.ts
+++ src/completable-promise.ts
@@ -1,2 +1,33 @@
-export class CompletablePromise {
+import { DeferredPromise } from "./deferred-promise";
+
+export class CompletablePromise<T = any> {
+
+  protected completablePromise!: DeferredPromise<T>;
+  private readonly promise: Promise<T>;
+
+  constructor() {
+    this.promise = new Promise<T>((resolve, reject) => {
+      this.completablePromise = { resolve: resolve, reject: reject };
+    });
+  }
+
+  resolve(value: T) {
+    this.completablePromise.resolve(value);
+  }
+
+  reject(reason: any) {
+    this.completablePromise.reject(reason);
+  }
+
+  then(onfulfilled?: ((value: T) => T | PromiseLike<T>) | null | undefined): Promise<T> {
+    return this.promise.then(onfulfilled);
+  }
+
+  catch(onrejected?: ((reason: any) => T | PromiseLike<T>) | undefined | null): Promise<T> {
+    return this.promise.catch(onrejected);
+  }
+
+  get() {
+    return this.promise;
+  }
 }
