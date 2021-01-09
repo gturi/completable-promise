90b2294ce9547cda67a4d89500c9843394b154a6
FlamingTuri <giacomo.venturini3@gmail.com>
2021-01-09 18:57:34 +0100

    onfulfilled and onrejected now consider all the Promise<T> possible types

--- src/completable-promise.ts
+++ src/completable-promise.ts
@@ -21,11 +21,16 @@ export class CompletablePromise<T = any> {
     this.completablePromise.reject(reason);
   }
 
-  then<TResult1 = T, TResult2 = never>(onfulfilled?: ((value: T) => TResult1 | PromiseLike<TResult1>) | null | undefined): Promise<TResult1 | TResult2> {
-    return this.promise.then(onfulfilled);
+  then<TResult1 = T, TResult2 = never>(
+    onfulfilled?: ((value: T) => TResult1 | PromiseLike<TResult1>) | null | undefined,
+    onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null
+  ): Promise<TResult1 | TResult2> {
+    return this.promise.then(onfulfilled, onrejected);
   }
 
-  catch(onrejected?: ((reason: any) => T | PromiseLike<T>) | undefined | null): Promise<T> {
+  catch<TResult = never>(
+    onrejected?: ((reason: any) => TResult | PromiseLike<TResult>) | undefined | null
+  ): Promise<T | TResult> {
     return this.promise.catch(onrejected);
   }
 
